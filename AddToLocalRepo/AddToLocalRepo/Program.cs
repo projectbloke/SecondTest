ef5ac194b88b46f81f880ad5eff04d1b25f922d8
projectbloke <markreilly_007@hotmail.com>
2015-12-16 12:34:20 +0000

    Can't forget to push a change - initial commit here

---
+++ AddToLocalRepo/AddToLocalRepo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AddToLocalRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // so it wouldn't allow me to open the repo on Git
+            // and I guess you need to add to local repo before syncing...
+            // ... I'll see...
+
+        }
+    }
+}
